1c505837d4e97d1475e9428f6e438a8a680613a1
nkhur <145411496+nkhur@users.noreply.github.com>
2025-05-30 00:11:30 +0530

    link fix

--- src/app/components/navigation-bar.js
+++ src/app/components/navigation-bar.js
@@ -8,10 +8,9 @@ export default function Navbar({ refs }) {
       alignItems: "flex-start",
       gap: "1rem",
       paddingTop: "9%",
-      // paddingBottom:"5%"
     }}>
       <button onClick={() => refs.about.current.scrollIntoView({ behavior: "smooth" })}>\ about</button>
-      <button onClick={() => refs.work_experience.current.scrollIntoView({ behavior: "smooth" })}>\ work-experience</button>
+      <button onClick={() => refs.workExperience.current.scrollIntoView({ behavior: "smooth" })}>\ work-experience</button>
       <button onClick={() => refs.projects.current.scrollIntoView({ behavior: "smooth" })}>\ projects</button>
     </nav>
   );
