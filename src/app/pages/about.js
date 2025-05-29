f65b74f14d53d9d33e34b3ff877d1adf29d67451
nkhur <145411496+nkhur@users.noreply.github.com>
2025-05-29 23:12:47 +0530

    about page

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -6,6 +6,20 @@ export default function AboutPage() {
       <div>
         <h2>\ about</h2>
       </div>
+
+      <div>
+        <p>
+          interested in building tools that makes life better, not just faster. & designs that make tech feel alive. <br />
+          always curious, always learning, always building. 
+          <br /> <br />
+          beyond work, i’m usually deep in a playlist, swapping music recs with friends, or watching an f1 race. <br />
+          {/* i'm drawn to music, meaning, and the people who bring both. */}
+        </p>
+
+        <button className="contact-me-button" onClick={() => window.location.href = `mailto:nkhurana@umd.edu`}>say hi!</button>
+        <button className="github-button" onClick={() => window.open('https://github.com/nkhur', '_blank')}></button>
+        <button className="linkedin-button" onClick={() => window.open('https://linkedin.com/in/navya-khurana', '_blank')}></button>
+      </div>
       </>
     )
   }  
