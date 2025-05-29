e995f70c35a4f9ce714197deb42941e943da4591
nkhur <145411496+nkhur@users.noreply.github.com>
2025-05-30 01:43:12 +0530

    updated

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -12,7 +12,7 @@ export default function AboutPage() {
           interested in building tools that make life better, not just faster. & designs that make tech feel alive. <br />
           always curious, always learning, always building. 
           <br /> <br />
-          beyond work, i’m usually deep in a playlist, swapping music recs with friends, or watching an f1 race. <br /><br />
+          beyond work, i’m usually deep in a playlist, swapping music recs with friends, or watching an f1 race. or occasionally playing mario kart.<br /><br />
         </p>
 
         <button className="contact-me-button" onClick={() => window.location.href = `mailto:nkhurana@umd.edu`}>say hi!</button>
